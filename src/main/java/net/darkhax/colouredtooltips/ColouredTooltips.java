42408f54f1e192a578cc1d373ff42036712a7bf9
Tyler Hancock <darklime@live.ca>
2020-11-13 04:50:12 -0700

    Ported to 1.16.2.

--- src/main/java/net/darkhax/colouredtooltips/ColouredTooltips.java
+++ src/main/java/net/darkhax/colouredtooltips/ColouredTooltips.java
@@ -4,9 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.RenderTooltipEvent;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig.Type;
@@ -14,27 +11,15 @@ import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod("colouredtooltips")
 public class ColouredTooltips {
-
-	public static final Logger LOG = LogManager.getLogger("Coloured Tooltips");
-    public static ConfigurationHandler config = new ConfigurationHandler();
-
+    
+    public static final Logger LOG = LogManager.getLogger("Coloured Tooltips");
+    public static final ConfigurationHandler CONFIG = new ConfigurationHandler();
+    
     public ColouredTooltips() {
-    	
-    	if (FMLEnvironment.dist == Dist.CLIENT) {
-    		
-        	ModLoadingContext.get().registerConfig(Type.CLIENT, config.getSpec());
-    		MinecraftForge.EVENT_BUS.addListener(EventPriority.LOWEST, this::onTooltipColour);
-    	}
-    }
-
-    private void onTooltipColour (RenderTooltipEvent.Color event) {
-
-        // Check to see if tooltip colour was modified at all. If it has, we wont touch it.
-        if (event.getOriginalBackground() == event.getBackground() && event.getOriginalBorderEnd() == event.getBorderEnd() && event.getOriginalBorderStart() == event.getBorderStart()) {
-
-            event.setBorderStart(config.getStartColor());
-            event.setBorderEnd(config.getEndColor());
-            event.setBackground(config.getBackgroundColor());
+        
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            
+            ModLoadingContext.get().registerConfig(Type.CLIENT, CONFIG.getSpec());
         }
     }
 }
--- src/main/java/net/darkhax/colouredtooltips/ConfigurationHandler.java
+++ src/main/java/net/darkhax/colouredtooltips/ConfigurationHandler.java
@@ -4,7 +4,7 @@ import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ForgeConfigSpec.ConfigValue;
 
 public class ConfigurationHandler {
-
+    
     private final ForgeConfigSpec spec;
     
     // #505000ff (80, 0, 255, 80)
@@ -16,57 +16,57 @@ public class ConfigurationHandler {
     // #f0100010 (16, 0, 16, 240)
     private final ConfigValue<String> background;
     
-    public ConfigurationHandler () {
-
+    public ConfigurationHandler() {
+        
         final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
-
+        
         // General Configs
         builder.comment("General settings for the mod.");
         builder.push("general");
-
+        
         builder.comment("The color at the top of the tooltip gradient.");
-        borderStart = builder.define("borderStart", "505000ff");
+        this.borderStart = builder.define("borderStart", "505000ff");
         
         builder.comment("The color at the bottom of the tooltip gradient.");
-        borderEnd = builder.define("borderEnd", "5028007f");
+        this.borderEnd = builder.define("borderEnd", "5028007f");
         
         builder.comment("The color for the background of the tooltip.");
-        background = builder.define("background", "f0100010");
+        this.background = builder.define("background", "f0100010");
         
         this.spec = builder.build();
     }
     
-    public ForgeConfigSpec getSpec() {
-    	
-    	return this.spec;
+    public ForgeConfigSpec getSpec () {
+        
+        return this.spec;
     }
     
-    public int getStartColor() {
-    	
-    	return (int) getColor(this.borderStart);
+    public int getStartColor () {
+        
+        return (int) this.getColor(this.borderStart);
     }
     
-    public int getEndColor() {
-    	
-    	return (int) getColor(this.borderEnd);
+    public int getEndColor () {
+        
+        return (int) this.getColor(this.borderEnd);
     }
     
-    public int getBackgroundColor() {
-    	
-    	return (int) getColor(this.background);
+    public int getBackgroundColor () {
+        
+        return (int) this.getColor(this.background);
     }
     
-    private long getColor(ConfigValue<String> stringValue) {
-    	
-    	try {
-    		
-    		return Long.decode("0x" + stringValue.get());
-    	}
-    	
-    	catch (NumberFormatException e) {
-    		
-    		ColouredTooltips.LOG.error("The color value in the configuration file is not valid. The color white will be used instead. This is something the pack dev needs to fix.", e);
-    		return 0xffffffff;
-    	}
+    private long getColor (ConfigValue<String> stringValue) {
+        
+        try {
+            
+            return Long.decode("0x" + stringValue.get());
+        }
+        
+        catch (final NumberFormatException e) {
+            
+            ColouredTooltips.LOG.error("The color value in the configuration file is not valid. The color white will be used instead. This is something the pack dev needs to fix.", e);
+            return 0xffffffff;
+        }
     }
 }
