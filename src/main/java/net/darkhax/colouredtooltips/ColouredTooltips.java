ddbab8559d74a8de8f933f68eecbda080967214c
Tyler Hancock <darklime@live.ca>
2020-11-13 20:27:07 -0700

    Ported to 1.16.3

--- src/main/java/net/darkhax/colouredtooltips/ColouredTooltips.java
+++ src/main/java/net/darkhax/colouredtooltips/ColouredTooltips.java
@@ -4,6 +4,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderTooltipEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig.Type;
@@ -13,13 +16,25 @@ import net.minecraftforge.fml.loading.FMLEnvironment;
 public class ColouredTooltips {
     
     public static final Logger LOG = LogManager.getLogger("Coloured Tooltips");
-    public static final ConfigurationHandler CONFIG = new ConfigurationHandler();
+    private final ConfigurationHandler config = new ConfigurationHandler();
     
     public ColouredTooltips() {
         
         if (FMLEnvironment.dist == Dist.CLIENT) {
             
-            ModLoadingContext.get().registerConfig(Type.CLIENT, CONFIG.getSpec());
+            ModLoadingContext.get().registerConfig(Type.CLIENT, this.config.getSpec());
+            MinecraftForge.EVENT_BUS.addListener(EventPriority.LOWEST, this::onTooltipColour);
+        }
+    }
+    
+    private void onTooltipColour (RenderTooltipEvent.Color event) {
+        
+        // Check to see if tooltip colour was modified at all. If it has, we wont touch it.
+        if (event.getOriginalBackground() == event.getBackground() && event.getOriginalBorderEnd() == event.getBorderEnd() && event.getOriginalBorderStart() == event.getBorderStart()) {
+            
+            event.setBorderStart(this.config.getStartColor());
+            event.setBorderEnd(this.config.getEndColor());
+            event.setBackground(this.config.getBackgroundColor());
         }
     }
 }
