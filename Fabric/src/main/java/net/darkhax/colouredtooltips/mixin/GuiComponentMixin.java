9c603ff89a37cb0462c69ea6c8e3a3cda71a1deb
Tyler Hancock <darklime@live.ca>
2022-01-31 10:46:26 -0700

    Fix potential mixin conflicts.

--- Fabric/src/main/java/net/darkhax/colouredtooltips/mixin/GuiComponentMixin.java
+++ Fabric/src/main/java/net/darkhax/colouredtooltips/mixin/GuiComponentMixin.java
@@ -9,23 +9,13 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 @Mixin(GuiComponent.class)
 public class GuiComponentMixin {
 
-    @ModifyVariable(
-            method = "fillGradient(Lcom/mojang/math/Matrix4f;Lcom/mojang/blaze3d/vertex/BufferBuilder;IIIIIII)V",
-            at = @At("HEAD"),
-            ordinal = 5,
-            argsOnly = true
-    )
-    private static int create$replaceA(int originalA) {
+    @ModifyVariable(method = "fillGradient(Lcom/mojang/math/Matrix4f;Lcom/mojang/blaze3d/vertex/BufferBuilder;IIIIIII)V", at = @At("HEAD"), ordinal = 5, argsOnly = true)
+    private static int colouredtooltips$replaceA(int originalA) {
         return ColouredTooltipsFabric.getReplacementColor(originalA);
     }
 
-    @ModifyVariable(
-            method = "fillGradient(Lcom/mojang/math/Matrix4f;Lcom/mojang/blaze3d/vertex/BufferBuilder;IIIIIII)V",
-            at = @At("HEAD"),
-            ordinal = 6,
-            argsOnly = true
-    )
-    private static int create$replaceB(int originalB) {
+    @ModifyVariable( method = "fillGradient(Lcom/mojang/math/Matrix4f;Lcom/mojang/blaze3d/vertex/BufferBuilder;IIIIIII)V", at = @At("HEAD"), ordinal = 6, argsOnly = true)
+    private static int colouredtooltips$replaceB(int originalB) {
         return ColouredTooltipsFabric.getReplacementColor(originalB);
     }
 }
